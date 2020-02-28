647d2b07649faae676ccfce30d6c9421220a6013
Saif Ejaz <saif.ejaz@nagarro.com>
2020-02-28 09:42:22 +0530

    Crud App

---
+++ models/db.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/EmployeeDB',{ useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => console.log('Connected to MongoDB'))
+        .catch(err => console.error('Could not connect to MongoDB...', err));
+
+require('./employee.model');
